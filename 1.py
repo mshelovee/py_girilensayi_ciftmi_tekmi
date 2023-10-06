ecc0d6dede8436dd9b90e419e9ab807522dbe32a
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-06 21:49:52 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+#GİRİLEN SAYININ TEK Mİ ÇİFT Mİ OLDUĞUNU BULAN PROGRAM
+sayi=int(input("bir sayı giriniz :"))
+if(sayi%2==0):
+  print("girilen sayı -çifttir.")
+else:
+  print("girilen sayı tektir.")
